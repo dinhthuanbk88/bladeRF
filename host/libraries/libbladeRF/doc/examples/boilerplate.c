9318fc1e44282b5a956964175038942b2d758213
Jon Szymaniak <jon.szymaniak@gmail.com>
2015-05-17 22:48:22 -0400

    libbladeRF/doc: Finished device config/boilerplate doc page

--- host/libraries/libbladeRF/doc/examples/boilerplate.c
+++ host/libraries/libbladeRF/doc/examples/boilerplate.c
@@ -1,8 +1,10 @@
+/** [Full listing] */
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <libbladeRF.h>
 
+/** [struct module_config] */
 /* The RX and TX modules are configured independently for these parameters */
 struct module_config {
     bladerf_module module;
@@ -12,11 +14,13 @@ struct module_config {
     unsigned int samplerate;
 
     /* Gains */
-    bladerf_lna_gain rxlna;
+    bladerf_lna_gain rx_lna;
     int vga1;
     int vga2;
 };
+/** [struct module_config] */
 
+/** [configure_module] */
 int configure_module(struct bladerf *dev, struct module_config *c)
 {
     int status;
@@ -35,8 +39,65 @@ int configure_module(struct bladerf *dev, struct module_config *c)
         return status;
     }
 
+    status = bladerf_set_bandwidth(dev, c->module, c->bandwidth, NULL);
+    if (status != 0) {
+        fprintf(stderr, "Failed to set bandwidth = %u: %s\n",
+                c->bandwidth, bladerf_strerror(status));
+        return status;
+    }
+
+    switch (c->module) {
+        case BLADERF_MODULE_RX:
+            /* Configure the gains of the RX LNA, RX VGA1, and RX VGA2  */
+            status = bladerf_set_lna_gain(dev, c->rx_lna);
+            if (status != 0) {
+                fprintf(stderr, "Failed to set RX LNA gain: %s\n",
+                        bladerf_strerror(status));
+                return status;
+            }
+
+            status = bladerf_set_rxvga1(dev, c->vga1);
+            if (status != 0) {
+                fprintf(stderr, "Failed to set RX VGA1 gain: %s\n",
+                        bladerf_strerror(status));
+                return status;
+            }
+
+            status = bladerf_set_rxvga2(dev, c->vga2);
+            if (status != 0) {
+                fprintf(stderr, "Failed to set RX VGA2 gain: %s\n",
+                        bladerf_strerror(status));
+                return status;
+            }
+            break;
+
+        case BLADERF_MODULE_TX:
+            /* Configure the TX VGA1 and TX VGA2 gains */
+            status = bladerf_set_txvga1(dev, c->vga1);
+            if (status != 0) {
+                fprintf(stderr, "Failed to set TX VGA1 gain: %s\n",
+                        bladerf_strerror(status));
+                return status;
+            }
+
+            status = bladerf_set_txvga2(dev, c->vga2);
+            if (status != 0) {
+                fprintf(stderr, "Failed to set TX VGA2 gain: %s\n",
+                        bladerf_strerror(status));
+                return status;
+            }
+            break;
+
+        default:
+            status = BLADERF_ERR_INVAL;
+            fprintf(stderr, "%s: Invalid module specified (%d)\n",
+                    __FUNCTION__, c->module);
+
+    }
+
     return status;
 }
+/** [configure_module] */
 
 /* Usage:
  *   libbladeRF_example_boilerplate [serial #]
@@ -49,9 +110,11 @@ int configure_module(struct bladerf *dev, struct module_config *c)
 int main(int argc, char *argv[])
 {
     int status;
+    struct module_config config;
+
+    /** [Opening a device] */
     struct bladerf *dev = NULL;
     struct bladerf_devinfo dev_info;
-    struct module_config config;
 
     /* Initialize the information used to identify the desired device
      * to all wildcard (i.e., "any device") values */
@@ -70,13 +133,14 @@ int main(int argc, char *argv[])
 
         return 1;
     }
+    /** [Opening a device] */
 
     /* Set up RX module parameters */
     config.module     = BLADERF_MODULE_RX;
     config.frequency  = 910000000;
     config.bandwidth  = 2000000;
     config.samplerate = 300000;
-    config.rxlna      = BLADERF_LNA_GAIN_MAX;
+    config.rx_lna     = BLADERF_LNA_GAIN_MAX;
     config.vga1       = 30;
     config.vga2       = 3;
 
@@ -110,3 +174,4 @@ out:
     bladerf_close(dev);
     return status;
 }
+/** [Full listing] */
